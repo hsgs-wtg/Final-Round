423919b75dc8f0328c1be18e5dc1f655e671283f
Minh Nhật Nguyễn <86871862+minhnhatnoe@users.noreply.github.com>
2023-07-20 17:19:53 +0700

    Finalize constraints

--- src/constraints.py
+++ src/constraints.py
@@ -8,17 +8,23 @@ def constraint_suitable_jobs(model, vars, skills):
 # Constraint 2: A person cannot work right after the night shift
 
 def constraint_night_shift(model, vars):
-    pass
+    night_shifts = vars[:, 2:vars.shape[1]-1:3].sum(axis=-1)
+    first_shifts = vars[:, 5::3].sum(axis=-1)
+    model.addConstr(night_shifts + first_shifts <= 1)
 
 # Constraint 3: A person can only do one shift in one day
 
 def constraint_one_shift(model, vars):
-    pass
+    shifts1 = vars[:, 0::3].sum(axis=-1)
+    shifts2 = vars[:, 1::3].sum(axis=-1)
+    shifts3 = vars[:, 2::3].sum(axis=-1)
+    model.addConstr(shifts1 + shifts2 + shifts3 <= 1)
 
 # Constraint 4: A person can only work in 24 days
 
 def constraint_24_days(model, vars):
-    pass
+    persons = vars.sum(axis=(1, 2))
+    model.addConstr(persons <= 24)
 
 # Resolve constraints
 
--- src/consts.py
+++ src/consts.py
@@ -3,6 +3,7 @@ from pathlib import Path
 root_path = Path(__file__).parent.parent
 data_root_path = root_path / "data"
 
+ALLOWED_DAYS = 24
 SHIFTS = 28 * 3
 JOBS = 3 
 
