f0d46b1821e6fa8b0abd78452f78ba794eaf4b24
Unknown15082 <trangiahuy15082006@gmail.com>
2023-07-20 14:44:41 +0700

    Read skills

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,4 @@
-from consts import data_root_path
+from consts import data_root_path, JOBS, JOBLIST
 
 class Dataset:
     def __init__(self, folder):
@@ -26,6 +26,17 @@ class Dataset:
             f.readline() # Comment line
             _names = [line.strip().split() for line in f.readlines()]
 
-            self.names = dict([worker[1], int(worker[0])] for worker in _names)
+            self.names = dict([worker[1], int(worker[0]) - 1] for worker in _names)
 
             self.workers_count = len(self.names)
+            self.skills = [[0] * JOBS for _ in range(self.workers_count)]
+
+        # Load skills of workers from ky_nang_Day_chuyen_?_?.txt
+        for pipeline_idx in range(1, self.pipeline + 1):
+            for job_idx in range(JOBS):
+                job = JOBLIST[job_idx]
+                with open(self.data_path / f"ky_nang_Day_chuyen_{pipeline_idx}_{job}.txt", "r") as f:
+                    worker_list = [line.strip() for line in f.readlines()]
+                    for worker_name in worker_list:
+                        worker = self.names[worker_name]
+                        self.skills[worker][job_idx] = 1
