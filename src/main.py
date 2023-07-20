e34a7a4f29891ff40045b626ef87b7bedff80c92
Unknown15082 <trangiahuy15082006@gmail.com>
2023-07-20 22:10:40 +0700

    Actually running rn

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ import gurobipy as gp
 from gurobipy import GRB
 from pathlib import Path
 
-from consts import SHIFTS, JOBS
+from consts import SHIFTS, JOBS, JOBLIST
 from dataset import Dataset
 
-from constraints import resolve_constraints
+from constraints import resolve_constraints, constraint_cardinality
 from objective import add_objective_func
 
 data = None
@@ -16,10 +16,6 @@ def load_input():
     global data
     data = Dataset("duLieu1")
 
-    print(data.names)
-    print(data.skills)
-    print(data.shift_time)
-
 def create_aux_vars(model, vars):
     hire_vars = model.addMVar(shape = (data.workers_count), vtype = GRB.BINARY)
     
@@ -27,11 +23,36 @@ def create_aux_vars(model, vars):
 
     return hire_vars
 
+def run(model, vars):
+    r = 0
+    D = 3
+    for shift in range(SHIFTS):
+        # Check if this is the last shift of the day
+        if shift % 3 == 0:
+            while r < shift + D:
+                # Load r
+                constraint_cardinality(model, vars, data, r)
+                r += 1
 
+        # Process current shift
+        model.optimize()
+
+        # Lock current choice
+        model.addConstr(vars[:, shift, :, :] == vars.x[:, shift, :, :])
+
+def print_output(result):
+    for assignment in result:
+        person, shift, pipeline, job = assignment
+        day, group = shift // 3, shift % 3
+        print(f"{day+1:02d}.06.2023 Ca_{group+1} V{person+1:02d} Day_chuyen_{pipeline+1} {JOBLIST[job]}")
+    
 def main():
     load_input()
 
-    model = gp.Model("job_scheduling_01")
+    env = gp.Env(empty = True)
+    env.setParam('OutputFlag', 0)
+    env.start()
+    model = gp.Model("job_scheduling_01", env = env)
     
     vars = model.addMVar(shape = (data.workers_count, SHIFTS, data.pipeline, JOBS), vtype = GRB.BINARY)
 
@@ -40,7 +61,13 @@ def main():
     resolve_constraints(model, vars, data)
     add_objective_func(model, vars, vars_auxiliary, data)
 
-    model.optimize()
+    run(model, vars)
+
+    result = (vars.x + 0.2).astype(int).nonzero()
+    result_zip = list(zip(*result))
+
+    print_output(result_zip)
+
 
 if __name__ == "__main__":
     main()
