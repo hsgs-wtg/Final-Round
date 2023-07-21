7cc2fa5350a36637e9343eac820e7e06487eff65
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2023-07-22 02:13:14 +0700

    wip

---
+++ src/main_delta.py
@@ -0,0 +1,190 @@
+import numpy as np
+import gurobipy as gp
+from gurobipy import GRB
+from pathlib import Path
+import sys
+
+from consts import SHIFTS, JOBS, JOBLIST, root_path
+from dataset import Dataset
+
+from constraints import resolve_constraints, constraint_cardinality
+from objective import add_objective_func
+from balance_weighted import BalanceWeighted
+from balance import BalanceUnweighted, get_acceptance_function
+
+data = None
+vision = 1  # day(s)
+
+
+def load_input(test, subtask):
+    # Modify data
+    global data
+    data = Dataset(f"duLieu{test}", subtask)
+
+
+def create_aux_vars(model, vars):
+    hire_vars = model.addMVar(shape=(data.workers_count), vtype=GRB.BINARY)
+    model.addConstr(hire_vars * 24 >= vars.sum(axis=(1, 2, 3)))
+    return hire_vars
+
+
+dissat = None
+dissat_functions = None
+total_dissat = None
+
+def optimize_delta(model, vars, vars_auxiliary, acp):
+    last_accepted: np.ndarray = vars.x
+    iteration = 0
+    # breakpoint()
+    while True:
+        iteration += 1
+        print(f"{iteration = }")
+        avg_dissat, min_dissat, max_dissat = dissat.dissat_stats(
+            vars.x, vars_auxiliary)
+        print(dissat.dissat_stats(vars.x, vars_auxiliary))
+        delta = (max_dissat - min_dissat) * .9
+
+        lb = avg_dissat - delta/2
+        ub = avg_dissat + delta/2
+
+        constrs = []
+        for df in dissat_functions:
+            constrs.append(
+                model.addConstr(df <= ub)
+            )
+        for df in dissat_functions:
+            constrs.append(
+                model.addConstr(df >= lb)
+            )
+        model.optimize()
+        model.remove(constrs)
+
+        if model.status != GRB.OPTIMAL:
+            print(f"Broke due to {model.status = }, {delta = }")
+            break
+        if not acp():
+            print(f"Broke due to not accepted, {delta = }")
+            break
+        if delta < 0.1:
+            print(f"{delta = } is feasible")
+
+        last_accepted = vars.x
+    return last_accepted
+
+
+def run(model, vars, avg_shift, delta):
+    r = 0
+    D = vision * 3
+    acc = 0
+    constrs = []
+    def refresh_avg():
+        nonlocal constrs
+        model.remove(constrs)
+        constrs = []
+        for df in dissat_functions:
+            constrs.append(model.addConstr(df + delta >= acc))
+            constrs.append(model.addConstr(df - delta <= acc))
+        constrs.append(model.addConstr(total_dissat == acc))
+        
+    for shift in range(SHIFTS):
+        print(f"Calculating for {shift = }")
+        # Check if this is the last shift of the day
+        if shift % 3 == 0:
+            while r < shift + D and r < SHIFTS:
+                # Load r
+                constraint_cardinality(model, vars, data, r)
+                acc += avg_shift[r]
+                r += 1
+            # refresh_avg()
+
+        # Process current shift
+        model.optimize()
+
+        # Check for error code
+        if model.status == GRB.INFEASIBLE:
+            print("Solution not found")
+            exit(0)
+
+        # Lock current choice
+        model.addConstr(vars[:, shift, :, :] == (
+            vars.x[:, shift, :, :] + 0.2).astype(int))
+    model.optimize()
+
+
+def print_output(result, filename="result.txt"):
+    result.sort(key=lambda x: x[1])
+    answers = []
+    for assignment in result:
+        person, shift, pipeline, job = assignment
+        day, group = shift // 3, shift % 3
+        answers.append(
+            f"{day+1:02d}.06.2023 Ca_{group+1} V{person+1:02d} Day_chuyen_{pipeline+1} {JOBLIST[job]}")
+
+    with open(Path(root_path / "result" / filename), "w+") as f:
+        f.write("\n".join(answers))
+        f.write("\n")
+
+
+def usage():
+    print("Usage: python src/main.py DATA SUBTASK [VISION]")
+    print("DATA [1|2]: corresponding to duLieu1 and duLieu2")
+    print("SUBTASK [a|b]: corresponding to subtask a and b")
+    print("VISION [int]: number of days that can be see ahead")
+    exit(0)
+
+
+def main():
+
+    if len(sys.argv) < 3:
+        usage()
+
+    try:
+        test = sys.argv[1]
+        subtask = sys.argv[2]
+        if len(sys.argv) >= 4:
+            global vision
+            vision = int(sys.argv[3])
+        assert test in ('1', '2')
+        assert subtask in ('a', 'b')
+    except:
+        usage()
+
+    load_input(test, subtask)
+
+    env = gp.Env(empty=True)
+    # env.setParam('OutputFlag', 0)
+    env.start()
+    model = gp.Model(f"job_scheduling_{test}_{subtask}", env=env)
+
+    vars = model.addMVar(shape=(data.workers_count, SHIFTS,
+                         data.pipeline, JOBS), vtype=GRB.BINARY)
+
+    vars_auxiliary = create_aux_vars(model, vars)
+
+    resolve_constraints(model, vars, data)
+
+    global dissat, dissat_functions, total_dissat
+    dissat = BalanceWeighted(data)
+    dissat_functions = dissat.calculate_dissat(vars)
+    total_dissat = dissat_functions.sum()
+    
+    avg_shift = dissat.get_average(data)
+    delta = model.addVar(vtype = GRB.CONTINUOUS)
+
+    model.setObjective(delta, GRB.MINIMIZE)
+
+    run(model, vars, avg_shift, delta)
+
+    # run(model, vars, vars_auxiliary)
+
+    result = (vars.x + 0.2).astype(int).nonzero()
+    result_zip = list(zip(*result))
+
+    filename = f"result_data_{test}_part_{subtask}.txt"
+    print_output(result_zip, filename)
+
+    print(f"A solution is found (stored in {filename})")
+
+
+if __name__ == "__main__":
+    main()
