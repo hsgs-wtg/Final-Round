33ba7431675fe224a22256ad9d1fc256176f0675
Nguyễn Minh Nhật <86871862+minhnhatnoe@users.noreply.github.com>
2023-07-22 12:57:56 +0700

    Optimize delta (#8)
    * WIP
    
    * Finish weighted
    
    * wip
    
    * wip
    
    * Delta model
    
    * Different crit
    
    * checker
    
    * fix checker
    
    * Reconcile and gitignore
    
    ---------
    
    Co-authored-by: socpite <socpite@gmail.com>

--- src/balance_weighted.py
+++ src/balance_weighted.py
@@ -11,7 +11,14 @@ class BalanceWeighted:
         # pipeline | shift
         shift_time = data.shift_time
         # pipeline | shift
-        result = (shift_time + 2) * shift_dissat
+        result = np.ndarray(shift_time.shape)
+        for p in range(shift_time.shape[0]):
+            for s in range(shift_time.shape[1]):
+                if shift_time[p][s] != 0:
+                    result[p][s] = (shift_time[p][s] + 2) * shift_dissat[s]
+                else:
+                    result[p][s] = 0
+
         # shift | pipeline
         result = result.transpose((1, 0))
         # shift+pipeline
@@ -37,9 +44,9 @@ class BalanceWeighted:
         avg_dissat = cr_dissat.sum() / cr_size
         return avg_dissat, cr_dissat.min(), cr_dissat.max()
 
-    def get_average(self, data):
+    def get_tt_dissat(self, data):
         # pipeline
         pipeline_sum = data.pipeline_req.sum(axis=1)
         pc = pipeline_sum.shape[0]
         total = np.sum([self.shift_pl_dissat[p::pc].flatten() * pipeline_sum[p] for p in range(pc)], axis=0)
-        return total/data.workers_count
+        return total
