3c0bd45508d56dbce3636d32528c92d6b6344fe1
socpite <socpite@gmail.com>
2023-07-22 13:22:05 +0700

    update checker

--- checker.cpp
+++ checker.cpp
@@ -4,8 +4,16 @@ using namespace std;
 const int SHIFT_COUNT = 28*3;
 const int REQUIREMENT_COUNT = 3;
 
-const int TASK = 2;
-const int PIPELINE_COUNT = 3;
+const int PIPELINE_TASK[3] = {0, 1, 3};
+
+int TASK = 1;
+int PIPELINE_COUNT = 1;
+string SUBTASK = "a";
+
+const int HOURS_PER_SHIFT = 8;
+const int TASK = 1;
+const int PIPELINE_COUNT = 1;
+const int HOURS_PER_SHIFT = 1;
 const string SUBTASK = "a";
 
 const string FILE_DIR = "data/duLieu" + to_string(TASK) + "/";
@@ -23,11 +31,17 @@ map<string, int> JOB_INDEX = {
 
 const int TIME_FRAME[3] = {6, 14, 22};
 
+const int C_s = 1600000;
+const int C_h = 10000;
+
 struct Pipeline{
     vector<vector<vector<vector<bool>>>> x;
     vector<vector<vector<bool>>> A;
     vector<vector<int>> r;
     vector<vector<int>> Q;
+    vector<vector<float>> W;
+    vector<float> U;
+    vector<float> S;
     void read_number_of_people(){
         ifstream cin(FILE_DIR + "01_nhan_su.txt");
 
@@ -88,18 +102,54 @@ struct Pipeline{
         fgets(str, sizeof(str), ptr);
 
         Q.resize(SHIFT_COUNT);
+        W.resize(SHIFT_COUNT);
+
+        for(auto &vec: W)vec.assign(PIPELINE_COUNT, 0);
 
         int yy, mm, dd, hh, mn, ss;
         while(fscanf(ptr, "%d-%d-%d %d:%d:%d; ", &yy, &mm, &dd, &hh, &mn, &ss) > 0){
             int l = (dd - 2)*3 + 2;
             for(auto v: TIME_FRAME)l += (hh >= v);
+            int h1 = hh, d1 = dd;
             fscanf(ptr, "%d-%d-%d %d:%d:%d", &yy, &mm, &dd, &hh, &mn, &ss);
             int r = (dd-2)*3+2;
-
+            int h2 = hh, d2 = dd;
 
             for(auto v: TIME_FRAME)r += (hh > v);
             for(int i = l; i <= r; i++){
                 Q[i].push_back(index);
+            }
+
+            if(l == r){
+                if(d1 < d2)W[l][index] += float(24-h1+h2)/HOURS_PER_SHIFT;
+                else W[l][index] += float(h2-h1)/HOURS_PER_SHIFT;
+            }
+
+            else{
+                if(h1 >= 22){
+                    W[l][index] += float(30-h1)/HOURS_PER_SHIFT;
+                }
+                else{
+                    for(auto v: TIME_FRAME)if(v > h1){
+                        W[l][index] += float(v - h1)/HOURS_PER_SHIFT;
+                        break;
+                    }
+                }
+                l++;
+                if(h2 <= 6){
+                    W[r][index] += float(h2+2)/HOURS_PER_SHIFT;
+                }
+                else{
+                    for(int i = 0; i < 3; i++){
+                        if(TIME_FRAME[2-i] < h2){
+                            W[r][index] += float(h2 - TIME_FRAME[2-i])/HOURS_PER_SHIFT;
+                            break;
+                        }
+                    }
+                }
+                r--;
+                for(int i = l; i <= r; i++)W[i][index] += 8/HOURS_PER_SHIFT;
+                
             }
 
             //cout << "Schedule: " << index << " " << l << " " << r << endl;
@@ -151,7 +201,9 @@ struct Pipeline{
                         sum += x[i][j][k][l] + x[i][j+1][k][l];
                     }
                 }
-                if(sum > 1)return false;
+                if(sum > 1){
+                    return false;
+                }
             }
         }
         return true;
@@ -210,7 +262,7 @@ struct Pipeline{
                     }
                     if(r[Q[j][k]][l] > sum)return false;
                     else if(r[Q[j][k]][l] < sum){
-                        cout << "bruh" << endl;
+                        cout << "Number of worker exeeded" << endl;
                     }
                 }
             }
@@ -231,6 +283,11 @@ struct Pipeline{
             read_schedule(i);
         }
 
+        for(int i = 0; i < SHIFT_COUNT; i++){
+            for(int j = 0; j < PIPELINE_COUNT; j++)cout << W[i][j] << " ";
+        }
+        cout << endl;
+
         cout << "Finished reading schedule" << endl;
         
         for(int i = 0; i < PIPELINE_COUNT; i++){
@@ -250,7 +307,6 @@ struct Pipeline{
 
         while(scanf("%d.%d.%d Ca_%d V%d Day_chuyen_%d %s", &dd, &mm, &yy, &shift, &index, &pipeline, c) > 0){
             string job(c);
-            //cout << dd << endl;
             x[index-1][(dd - 1)*3 + shift - 1][pipeline-1][JOB_INDEX[job]] = 1;
         }
     }
@@ -285,12 +341,76 @@ struct Pipeline{
         return good;
     }
 
+    void dissat_check(){
+        cout << "dissat_values: " << endl;
+        U.assign(SHIFT_COUNT, 1);
+        for(int i = 6; i+5 < SHIFT_COUNT; i+=21){
+            for(int j = i; j < i+3; j++)U[j]*=1.1;
+            for(int j = i+3; j < i+6; j++)U[j]*=1.7;
+        }
+
+        for(int i = 2; i < SHIFT_COUNT; i+=3){
+            U[i]*=1.5;
+        }
+
+        //for(auto v: U)cout << v << endl;
+
+
+
+        for(int i = 0; i < x.size(); i++){
+            float sum = 0;
+            for(int j = 0; j < SHIFT_COUNT; j++){
+                for(int k = 0; k < PIPELINE_COUNT; k++){
+                    for(int l = 0; l < REQUIREMENT_COUNT; l++){
+                        if(x[i][j][k][l]){
+                            sum += x[i][j][k][l]*(W[j][k] + 2.0/HOURS_PER_SHIFT)*U[j];
+                        }
+                    }
+                }
+            }
+            S.push_back(sum);
+        }
+        sort(S.begin(), S.end());
+
+        float avg = accumulate(S.begin(), S.end(), 0.0)/S.size();
+
+        cout << "sum = " << avg*S.size() << endl;
+
+        cout << "Min = " << S[0] << endl;
+        cout << "Max = " << S.back() << endl;
+        cout << "Min - Max = " << S.back() - S[0] << endl;
+        cout << "Avg = " << avg << endl;
+        cout << "delta = " << max(avg - S[0], S.back() - avg) << endl;
+    }
+
+    int get_cost(){
+        int total_cost = 0;
+        for(int i = 0; i < x.size(); i++){
+            int sum = 0;
+            for(int j = 0; j < SHIFT_COUNT; j++){
+                for(int k = 0; k < PIPELINE_COUNT; k++){
+                    for(int l = 0; l < REQUIREMENT_COUNT; l++){
+                        sum += x[i][j][k][l];
+                        total_cost += x[i][j][k][l]*W[j][k]*C_h;
+                    }
+                }
+            }
+            if(sum)total_cost += C_s;
+        }
+        return total_cost;
+    }
+
 };
 
 Pipeline pipeline;
 
-int main(){
+int main(int argc, char* argv[]){
+    TASK = atoi(argv[1]);
+    SUBTASK = argv[2];
+    PIPELINE_COUNT = PIPELINE_TASK[TASK];
     pipeline.read_input();
     pipeline.read_result();
     cout << pipeline.check() << endl;
+    pipeline.dissat_check();
+    cout << "Cost = " << pipeline.get_cost() << endl;
 }
